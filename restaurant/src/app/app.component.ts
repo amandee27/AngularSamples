ccf86f540a1ffe20f0994b7f83cab23b1ed0d24f
Amandee Ellawala <amandee.manushika.e@gmail.com>
2021-07-12 15:33:42 +0530

    Add search recipe in nav bar

--- restaurant/src/app/app.component.ts
+++ restaurant/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'restaurant';
+  recipeItem: string = '';
+  constructor(private route: Router) {}
+  searchRecipie(searchRecipe: string) {
+    this.recipeItem = searchRecipe;
+    this.route.navigate(['/recepies'], { queryParams: { q: searchRecipe } });
+  }
 }
